78bda9ca037a199013247683852b532ae6a0d013
SJH14o3 <sajjadjh1403@gmail.com>
2023-04-15 15:33:20 +0330

    fixed the movement Buttons bug and reduced the code size of them

--- src/Game.java
+++ src/Game.java
@@ -36,170 +36,120 @@ public class Game {
         }
         return false;
     }
+    private void turnFinished() {
+        if (d.turn == 1) {
+            d.turn++;
+        }
+        else {
+            d.turn--;
+        }
+        System.out.println("turn for " + d.turn);
+        System.out.println("i = " + i + "\n");
+        i = 0;
+    }
     public void MovementActions(){
         i=0;
             f.m.Left.addActionListener(new ActionListener() {
                 //TODO make error messages
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (d.turn == 1 && players[0].y > 0 && d.DiceNumber>0 && CheckNotePlaces("Left")==false) {
-                        players[0].y = (byte) (players[0].y - 1);
-                        NotePlaces[i][0]=players[0].x;
-                        NotePlaces[i][1]=players[0].y;
+                    if (players[d.turn-1].y > 0 && d.DiceNumber>0 && CheckNotePlaces("Left")==false) {
+                        players[d.turn-1].y = (byte) (players[d.turn-1].y - 1);
+                        NotePlaces[i][0]=players[d.turn-1].x;
+                        NotePlaces[i][1]=players[d.turn-1].y;
                         i++;
                         d.DiceNumber--;
-                        System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
+                        System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
                         System.out.println("DiceNumber: "+d.DiceNumber);
                         MovesLeft.setText("Moves Left: "+d.DiceNumber);
                         if (d.DiceNumber==0){
                             d.dice.setEnabled(true);
                             d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=2;
-                            System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
+                            System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
+                            MovesLeft.setText("Roll the dice");
+                            turnFinished();
                         }
-                    } else if (d.turn == 2 && players[1].y > 0 && d.DiceNumber>0 && CheckNotePlaces("Left")==false) {
-                        players[1].y = (byte) (players[1].y - 1);
-                        NotePlaces[i][0]=players[1].x;
-                        NotePlaces[i][1]=players[1].y;
-                        i++;
-                        d.DiceNumber--;
-                        System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                        System.out.println("DiceNumber: "+d.DiceNumber);
-                        MovesLeft.setText("Moves Left: "+d.DiceNumber);
-                        if (d.DiceNumber==0){
-                            d.dice.setEnabled(true);
-                            d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=1;
-                            System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
-                        }
+                    }
+                    else {
+                        System.out.println("failed");
                     }
                 }
             });
             f.m.Right.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (d.turn == 1 && players[0].y < 9 && d.DiceNumber>0 && CheckNotePlaces("Right")==false) {
-                        players[0].y = (byte) (players[0].y + 1);
+                    if (players[d.turn-1].y < 9 && d.DiceNumber>0 && CheckNotePlaces("Right")==false) {
+                        players[d.turn-1].y = (byte) (players[d.turn-1].y + 1);
                         NotePlaces[i][0]=players[0].x;
                         NotePlaces[i][1]=players[0].y;
                         i++;
                         d.DiceNumber--;
-                        System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
+                        System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
                         System.out.println("DiceNumber: "+d.DiceNumber);
                         MovesLeft.setText("Moves Left: "+d.DiceNumber);
                         if (d.DiceNumber==0){
                             d.dice.setEnabled(true);
                             d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=2;
-                            System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
+                            System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
+                            MovesLeft.setText("Roll the dice");
+                            turnFinished();
                         }
-                    } else if (d.turn == 2 && players[1].y < 9 && d.DiceNumber>0 && CheckNotePlaces("Right")==false) {
-                        players[1].y = (byte) (players[1].y + 1);
-                        NotePlaces[i][0]=players[1].x;
-                        NotePlaces[i][1]=players[1].y;
-                        i++;
-                        d.DiceNumber--;
-                        System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                        System.out.println("DiceNumber: "+d.DiceNumber);
-                        MovesLeft.setText("Moves Left: "+d.DiceNumber);
-                        if (d.DiceNumber==0){
-                            d.dice.setEnabled(true);
-                            d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=1;
-                            System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
-                        }
+                    }
+                    else {
+                        System.out.println("failed");
                     }
                 }
             });
             f.m.Up.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (d.turn == 1 && players[0].x > 0 && players[0].y>-1 && d.DiceNumber>0 && CheckNotePlaces("Up")==false) {
-                        players[0].x = (byte) (players[0].x - 1);
-                        NotePlaces[i][0]=players[0].x;
-                        NotePlaces[i][1]=players[0].y;
+                    if (players[d.turn-1].x > 0 && players[d.turn-1].y>-1 && d.DiceNumber>0 && CheckNotePlaces("Up")==false) {
+                        players[d.turn-1].x = (byte) (players[d.turn-1].x - 1);
+                        NotePlaces[i][0]=players[d.turn-1].x;
+                        NotePlaces[i][1]=players[d.turn-1].y;
                         i++;
                         d.DiceNumber--;
-                        System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
+                        System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
                         System.out.println("DiceNumber: "+d.DiceNumber);
                         MovesLeft.setText("Moves Left: "+d.DiceNumber);
                         if (d.DiceNumber==0){
                             d.dice.setEnabled(true);
                             d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=2;
-                            System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
+                            System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
+                            MovesLeft.setText("Roll the dice");
+                            turnFinished();
                         }
-                    } else if (d.turn == 2 && players[1].x > 0 && d.DiceNumber>0 && CheckNotePlaces("Up")==false) {
-                        players[1].x = (byte) (players[1].x - 1);
-                        NotePlaces[i][0]=players[1].x;
-                        NotePlaces[i][1]=players[1].y;
-                        i++;
-                        d.DiceNumber--;
-                        System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                        System.out.println("DiceNumber: "+d.DiceNumber);
-                        MovesLeft.setText("Moves Left: "+d.DiceNumber);
-                        if (d.DiceNumber==0){
-                            d.dice.setEnabled(true);
-                            d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=1;
-                            System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
-                        }
+                    }
+                    else {
+                        System.out.println("failed");
                     }
                 }
             });
             f.m.Down.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (d.turn == 1 && players[0].x < 9 && d.DiceNumber>0 && CheckNotePlaces("Down")==false) {
-                        players[0].x = (byte) (players[0].x + 1);
-                        NotePlaces[i][0]=players[0].x;
-                        NotePlaces[i][1]=players[0].y;
+                    if (players[d.turn-1].x < 9 && d.DiceNumber>0 && CheckNotePlaces("Down")==false) {
+                        players[d.turn-1].x = (byte) (players[d.turn-1].x + 1);
+                        NotePlaces[i][0]=players[d.turn-1].x;
+                        NotePlaces[i][1]=players[d.turn-1].y;
                         i++;
                         d.DiceNumber--;
-                        System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
-                        System.out.println("DiceNumber: "+d.DiceNumber);
-                        MovesLeft.setText("Moves Left: "+d.DiceNumber);
-                        if (d.DiceNumber==0){
-                            d.dice.setEnabled(true);
-                            d.turn=2;
-                            d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            System.out.println("player1(x,y): "+players[0].x+" "+players[0].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
-                        }
-                    } else if (d.turn == 2 && players[1].x < 9 && players[1].y<10 && d.DiceNumber>0 && CheckNotePlaces("Down")==false) {
-                        players[1].x = (byte) (players[1].x + 1);
-                        NotePlaces[i][0]=players[1].x;
-                        NotePlaces[i][1]=players[1].y;
-                        i++;
-                        d.DiceNumber--;
-                        System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
+                        System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
                         System.out.println("DiceNumber: "+d.DiceNumber);
                         MovesLeft.setText("Moves Left: "+d.DiceNumber);
                         if (d.DiceNumber==0){
                             d.dice.setEnabled(true);
                             d.dice.setIcon(new ImageIcon("images\\FirstDice.png"));
-                            d.turn=1;
-                            System.out.println("player2(x,y): "+players[1].x+" "+players[1].y);
-                            System.out.println("turn: "+d.turn);
-                            MovesLeft.setText("Role the dice");
+                            System.out.println("player(x,y): "+players[d.turn-1].x+" "+players[d.turn-1].y);
+                            MovesLeft.setText("Roll the dice");
+                            turnFinished();
                         }
+                    }
+                    else {
+                        System.out.println("failed");
                     }
                 }
             });
-
     }
 
     private void setupPlayers(byte count) {
@@ -207,14 +157,34 @@ public class Game {
         for (byte i = 0; i < players.length; i++) {
             players[i] = new Player();
             if (i==0 || i==2){
-                players[i].x=9;
-                players[i].y=-1;
+                players[i].x=-1;
+                players[i].y=9;
             } else if (i==1 || i==3) {
-                players[i].x=0;
-                players[i].y=10;
+                players[i].x=10;
+                players[i].y=0;
             }
             System.out.println(players[i].getName());
         }
+    }
+    private void GameLoop() {
+        d.turn = 1;
+        System.out.println("player1(x,y): " + players[d.turn-1].x + "  " + players[d.turn-1].y);
+        d.dice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                d.DiceNumber = (byte) ((byte) (Math.random() * d.rang) + d.min);
+                d.setTarget(d.DiceNumber);
+                d.dice.setIcon(new ImageIcon(d.target));
+                d.dice.setEnabled(false);
+                MovesLeft.setText("Moves Left: " + d.DiceNumber);
+                /*for (int k = 0; k < NotePlaces.length; k++) {
+                    for (int z = 0; z < NotePlaces[k].length; z++) {
+                        NotePlaces[k][z] = -2;
+                    }
+                }*/
+            }
+        });
+        MovementActions();
     }
     void questDone() {
         questNum--;
@@ -233,25 +203,6 @@ public class Game {
         MovesLeft.setVisible(true);
         MovesLeft.setOpaque(true);
         f.jl.add(MovesLeft, JLayeredPane.MODAL_LAYER);
-
-        d.turn=1;
-        System.out.println("player1(x,y): "+players[0].x+"  "+players[0].y);
-        System.out.println("player2(x,y): "+players[1].x+"  "+players[1].y);
-        d.dice.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                d.DiceNumber = (byte) ((byte) (Math.random() * d.rang) + d.min);
-                d.setTarget(d.DiceNumber);
-                d.dice.setIcon(new ImageIcon(d.target));
-                d.dice.setEnabled(false);
-                MovesLeft.setText("Moves Left: "+d.DiceNumber);
-                for (int k=0 ; k<NotePlaces.length ; k++){
-                    for (int z=0 ; z<NotePlaces[k].length ; z++){
-                        NotePlaces[k][z]=-2;
-                    }
-                }
-                MovementActions();
-            }
-        });
+        GameLoop();
     }
 }
